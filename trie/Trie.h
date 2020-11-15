a5ba007175f52a6c80a574b55e1a82f21eef5221
Johoyos <40185523+Johoyos@users.noreply.github.com>
2020-11-14 23:55:25 -0500

    Add files via upload

---
+++ trie/Trie.h
@@ -0,0 +1,103 @@
+#include "Node.h"
+#include <sys/stat.h>
+#include <fstream>
+using namespace std;
+class Trie{
+public:
+    Node * root;
+    int bytes;
+
+    Trie(){
+	root = new Node();
+    }
+    void insert(std::string str, int start, int offset){
+	Node**temp = &root;
+	for (char& c :str){
+	    //cout<< (int)c << endl;
+	    int posInArr = (int)c -32 ;
+	//	cout << c << " " << posInArr << endl;
+	    temp = &(*temp)->next[posInArr];
+
+	    if( !(*temp) ) {
+		//cout << "creating node" << endl;
+		*temp  =new Node();
+	    }
+	}
+	(*temp)->esTerminal = true;
+	(*temp)->addPath (std::make_pair (start, offset));
+    }
+
+
+    void dfs (Node **temp, std::string word) {
+	if ((*temp)->esTerminal) {
+	    std::unordered_map<int, std::vector<std::string>> sizes;
+	    std::cout << word<<"esta presente: " << std::endl;
+	    for (int i = 0; i < (*temp)->getPaths ().size (); i++) {
+		std::ifstream pathfile ("filePaths.txt");
+		char location [255];
+		pathfile.seekg ((*temp)->getPaths ()[i].first);
+		pathfile.read (location, (*temp)->getPaths ()[i].second);
+		struct stat buf;
+		stat (location, &buf);
+		sizes.insert(std::make_pair (buf.st_size, std::vector<std::string>()));
+		sizes[buf.st_size].push_back (location);
+	    }
+	    for (auto it = sizes.begin (); it != sizes.end (); it++) {
+		std::cout << "with size: " << it->first << " at ";
+		for (int i = 0; i < it->second.size (); i++) {
+			std::cout << it->second[i] << " | ";
+		}
+		std::cout << std::endl;
+	    }
+	}
+	bytes += sizeof(Node) + (2 * sizeof (int)) * (*temp)->paths.size ();
+	auto nodo = temp;
+	std::string palabra = word;
+
+	for (int i = 0; i < 94; i++) {
+		if ((*nodo)->next[i]) {
+			word += (char) (i + 32);
+			dfs (&(*nodo)->next[i], word);
+			word = palabra;
+		}
+	}
+    }
+
+    void consulta (std::string prefix) {
+	    Node **temp = &root;
+	    int pos;
+	    for (int i = 0; i < prefix.size (); i++) {
+		    pos = ((int) prefix[i]) - 32;
+		    if ((*temp)->next[pos])
+			    temp = &(*temp)->next[pos];
+		    else
+			    return;
+	    }
+	    dfs (temp, prefix);
+    }
+    int getBytes () {
+	return bytes;
+    }
+
+
+    void index (std::string fileName) {
+	    std::ifstream indexFile (fileName);
+	    std::string line, name;
+
+	    int start;
+	    start = indexFile.tellg ();
+
+	    while (std::getline (indexFile, line)) {
+		int i = line.size () - 1;
+		while (line[i] != '/') {
+			name = line[i] + name;
+			i--;
+		}
+		//cout<<name<<endl;
+		insert (name, start, line.size ());
+		name.clear ();
+		start = indexFile.tellg ();
+	    }
+    }
+
+};
--- trie/main.cpp
+++ trie/main.cpp
@@ -1,86 +1,38 @@
 #include <iostream>
 #include <string>
-
+#include <chrono>
+#include "Trie.h"
 using namespace std;
-struct Node{
-public:
-    Node* next[94]; // 32 al 126
-    bool esTerminal;
-
-    Node(){
-	esTerminal=false;
-	for (int i = 0; i < 94; i++) {
-		next[i] = nullptr;
-	}
-    }
-};
-
-class Trie{
-private:
-    Node * root;
-public:
-    Trie(){
-		root = new Node();
-    }
-    void insert(std::string str){
-	Node**temp = &root;
-	for (char& c :str){
-	    //cout<< (int)c << endl;
-	    int posInArr = (int)c -32 ;
-		cout << c << " " << posInArr << endl;
-	    temp = &(*temp)->next[posInArr];
-
-	    if( !(*temp) ) {
-		//cout << "creating node" << endl;
-		*temp  =new Node();
-	    }
-	}
-	(*temp)->esTerminal = true;
-    }
-    /*void printTrie(){
-	Node* imprimir = root;
-        std::string pal;
-        for(auto it = imprimir->next.begin(); it != imprimir->next.end(); ++it) {
-            pal.push_back(it->first);
-            print_helper(pal, it->second);
-            pal.clear();
-        }
-
-    }*/
-	
-	void dfs (Node *temp, std::string word) {
-		if (temp->esTerminal) {
-			std::cout << word << std::endl;
-		}
-		for (int i = 0; i < 94; i++) {
-			if (temp->next[i]) {
-				word += (char) (i + 32);
-				dfs (temp->next[i], word);
-			}
-		}
-	}
-
-	void withPrefix (std::string prefix) {
-		Node *temp = root;
-		int pos;
-		for (int i = 0; i < prefix.size (); i++) {
-			pos = ((int) prefix[i]) - 32;
-			if (temp->next[pos])
-				temp = temp->next[pos];
-			else
-				return;
-		}
-		dfs (temp, prefix);
-	}
-};
 
 int main(){
-    string str= " hola! #1";
-    string str2= "heyy";
-    Trie tr;
-    tr.insert(str);
-    tr.insert(str2);
-    tr.withPrefix ("h");
 
-   return 0;
+    std::cout << " INSERTA TRIE " << std::endl;
+    Trie trie;
+    trie.index("filePaths.txt");
+    /*
+    auto start = std::chrono::high_resolution_clock::now ();
+    for (int i = 0; i < 10; i++) {
+	    trie.consulta("Con");
+	    trie.consulta("Emergency");
+	    trie.consulta("jasper");
+	    trie.consulta("CS");
+	    trie.consulta("Software");
+	    trie.consulta("EDA");
+	    trie.consulta("Initial");
+	    trie.consulta("classified");
+	    trie.consulta("standard");
+	    trie.consulta("s");
+    }
+    auto end = std::chrono::high_resolution_clock::now ();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
+    std::cout << "duration: " << duration << "us" << std::endl;
+*/
+    std::cout << " BUSCA TRIE " << std::endl;
+    cout<<"Visuazilar archivos con conjunto de letras: ";
+    std::string var;
+    cin>>var ;
+    trie.consulta(var);
+
+    std::cout << "Size in bytes: " << trie.getBytes () << std::endl;
+    return 0;
 }
